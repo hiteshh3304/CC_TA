cc6b47915b2971a6934de382ffbc364b3f766483
hiteshh3304 <142288522+hiteshh3304@users.noreply.github.com>
2025-03-13 15:31:41 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,8 +3,8 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World\n";
-  cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run this  \n";
+  cout << "Hello, Worlld\n";
+  cout << "Hello, Jenkinsss\n";
+  cout << "I have successfully built and ran this  \n";
   return 0;
 }
